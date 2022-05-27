03c44cac1ea3aa3be63260115343fcd037f3ac42
Shawn11564 <thunderboy288@gmail.com>
2022-05-27 11:34:30 -0400

    Initial Commit

---
+++ src/test/java/com/summitworks/disasterrecovery/DisasterRecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summitworks.disasterrecovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterRecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
