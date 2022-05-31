083e3bf90524c1fac7766ac969e0858a3562b5b1
Shawn11564 <shawnmicheldev@gmail.com>
2022-05-31 19:18:15 -0400

    Add @CrossOrigin annotation to fix requests getting blocked from the frontend

--- src/main/java/com/summitworks/disasterrecovery/controllers/AuthController.java
+++ src/main/java/com/summitworks/disasterrecovery/controllers/AuthController.java
@@ -17,16 +17,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
