a09acb2b59ebee4dff01fe04a648d6e0b20e451e
Shawn11564 <thunderboy288@gmail.com>
2022-05-31 11:34:16 -0400

    Merge Matthew1, refactor variables

--- src/main/java/com/summitworks/disasterrecovery/models/objects/TimeSheetObject.java
+++ src/main/java/com/summitworks/disasterrecovery/models/objects/TimeSheetObject.java
@@ -1,41 +1,34 @@
 package com.summitworks.disasterrecovery.models.objects;
 
-import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DiscriminatorOptions;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.DiscriminatorOptions;
-
-import com.summitworks.disasterrecovery.models.users.Role;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
-@Table (name = "Time_Sheet_Approvals")
+@Table(name = "Time_Sheet_Approvals")
 @Getter
 @Setter
 @DiscriminatorOptions(force = true)
 public class TimeSheetObject {
 	@Id
-	int Id;
-	String Contractor_name;
-	String Site_code;
-	int Total_hours;
-	String Status;
-	
+	private int id;
+	private String contractorName;
+	private String siteCode;
+	private int totalHours;
+	private String status;
+
 	@Override
 	public String toString() {
-		return "SiteObject{" +
-				"Id='" + Id + '\'' +
-				", Contractor_name=" + Contractor_name +
-				", Site_code='" + Site_code + '\'' +
-				", Status=" + Status +
+		return "TimeSheetObject{" +
+				"id=" + id +
+				", contractorName='" + contractorName + '\'' +
+				", siteCode='" + siteCode + '\'' +
+				", totalHours=" + totalHours +
+				", status='" + status + '\'' +
 				'}';
 	}
-	
 }
