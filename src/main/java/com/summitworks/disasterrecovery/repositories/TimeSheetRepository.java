685a3835d5e6dbf832d5bfd63b23c1b98e02f3ab
Shawn11564 <thunderboy288@gmail.com>
2022-05-31 21:18:41 -0400

    Update mysql dialect

--- src/main/java/com/summitworks/disasterrecovery/repositories/TimeSheetRepository.java
+++ src/main/java/com/summitworks/disasterrecovery/repositories/TimeSheetRepository.java
@@ -1,14 +1,16 @@
 package com.summitworks.disasterrecovery.repositories;
 
-import java.util.List;
-
+import com.summitworks.disasterrecovery.models.objects.TimeSheetObject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.summitworks.disasterrecovery.models.objects.TimeSheetObject;
-import com.summitworks.disasterrecovery.models.users.User;
+import java.util.List;
 
 public interface TimeSheetRepository extends JpaRepository<TimeSheetObject, Long> {
 	@Query(value = "SELECT * from Time_Sheet_Approvals", nativeQuery = true)
 	List<TimeSheetObject> getAllTimeSheetObjects();
+
+	@Query(value = "SELECT * from Time_Sheet_Approvals WHERE contractor_name = ?1", nativeQuery = true)
+	List<TimeSheetObject> getContractorTimeSheetObjects(String name);
+
 }
