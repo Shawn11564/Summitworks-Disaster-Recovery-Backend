915e952355e30815b70c6c73370aa6749d939b6e
Kalavast <43184173+Kalavast@users.noreply.github.com>
2022-06-01 13:03:13 -0400

    a

--- src/test/java/com/summitworks/disasterrecovery/DisasterRecoveryApplicationTests.java
+++ src/test/java/com/summitworks/disasterrecovery/DisasterRecoveryApplicationTests.java
@@ -1,13 +1,13 @@
-package com.summitworks.disasterrecovery;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DisasterRecoveryApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.summitworks.disasterrecovery;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DisasterRecoveryApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
