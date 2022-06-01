b756af15c1d0905e24cc8d82db033ec842d81f2f
Kalavast <43184173+Kalavast@users.noreply.github.com>
2022-06-01 19:15:37 -0400

    a

--- src/main/java/com/summitworks/disasterrecovery/controllers/TimeSheetController.java
+++ src/main/java/com/summitworks/disasterrecovery/controllers/TimeSheetController.java
@@ -1,9 +1,24 @@
 package com.summitworks.disasterrecovery.controllers;
 
+<<<<<<< Updated upstream
 import lombok.AllArgsConstructor;
+=======
+import com.summitworks.disasterrecovery.controllers.requests.LoginRequest;
+import com.summitworks.disasterrecovery.controllers.responses.MessageResponse;
+import com.summitworks.disasterrecovery.services.TimeSheetService;
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+>>>>>>> Stashed changes
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
+import javax.validation.Valid;
 
 @RestController
 //@CrossOrigin(origin = "http://localhost:4200/")
@@ -16,4 +31,12 @@ public class TimeSheetController {
 		return "";
 	}
 	
+<<<<<<< Updated upstream
+=======
+	@PostMapping("/")
+	public ResponseEntity<?> inserttimesheet(@Valid @RequestBody LoginRequest loginRequest){
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+
+>>>>>>> Stashed changes
 }
